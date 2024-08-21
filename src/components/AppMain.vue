e66ed9242c11340bda74d8d02b0792c15b9bffdf
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-21 19:00:07 +0200

    added function to get projects via axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,39 @@
 <script>
+import axios from 'axios';
 export default{
     data(){
         return {
-            message: 'AppMain'
+            message: 'AppMain',
+            projects: [],
         }
+    },
+    methods: {
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            }); 
+        }
+    },
+    created(){
+        this.getProjects();
     }
 }
 </script>
 
 <template>
     <h1> {{ message }} </h1>
+    <ul>
+        <li v-for="project in projects" key="project.id"> {{ project.title }} </li>
+    </ul>
 </template>
 
 <style scoped lang="scss">
