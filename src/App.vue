ac13aada61b88f6356b58d4658a9d5fbe3f5655a
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-21 18:30:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script >
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+  export default {
+    data() {
+      return { 
+
+      }
+    },
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style scoped lang="scss">
+  @use '../node_modules/bootstrap/scss/bootstrap.scss';
+  @use './style/general.scss';
+  @use './style/partials/variables' as *;
+  @use './style/partials/mixins' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    data(){
+        return {
+            message: 'AppMain'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ message }} </h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
