4a3b714c6146b3a5d46196be307ebb01a33a3d86
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-22 23:08:20 +0200

    changed pagination link in ProjectList.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,10 +22,10 @@ export default{
 </script>
 
 <template>
-    <header class="border-bottom border-2 border-success px-3 py-1">
+    <header class="px-3 py-1">
         <nav class="navbar navbar-expand ">
             <div class="container-fluid">
-                <a class="navbar-brand text-success text-uppercase fw-bold" href="#">Boolfolio</a>
+                <a class="navbar-brand text-uppercase fw-bold" href="#">Boolfolio</a>
                 
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item" v-for="link in navLinks">
@@ -38,5 +38,13 @@ export default{
 </template>
 
 <style scoped lang="scss">
-    
+    @use '../style/partials/variables' as *;
+
+    header{
+        border-bottom: 2px solid $primary-dark;
+
+        a.navbar-brand{
+            color: $primary-dark;
+        }
+    }
 </style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -7,7 +7,9 @@ export default{
         return {
             projects: [],
             pageIndex: [],
-            currentPage: ''
+            currentPage: '',
+            lastPage: '',
+            visiblePages: []
         }
     },
     components:{
@@ -23,9 +25,9 @@ export default{
             .then((response) => {
                 console.log(response.data.results);
                 this.projects = response.data.results.data;
-                const lastPage = response.data.results.last_page;
+                this.lastPage = response.data.results.last_page;
                 this.pageIndex = [];
-                for (let index = 1; index <= lastPage; index++){
+                for (let index = 1; index <= this.lastPage; index++){
                     this.pageIndex.push(index);
                 }
                 this.currentPage = response.data.results.current_page;
@@ -34,6 +36,18 @@ export default{
                 this.$router.push({ name: '404-not-found' });
                 console.log(error);
             }); 
+        },
+        nextPage(){
+            if(this.currentPage < this.lastPage){
+                this.currentPage += 1;
+                this.getProjects(this.currentPage);
+            }
+        },
+        prevPage(){
+            if(this.currentPage > 1){
+                this.currentPage = this.currentPage - 1;
+                this.getProjects(this.currentPage);
+            }
         }
     },
     created(){
@@ -51,17 +65,20 @@ export default{
                     <ProjectCard class="col-8" :title="project.title" :user="project.user" :date="project.date" :type="project.type" :description="project.description"/>
                 </router-link>
 
-                <!-- pageIndex -->
+                <!-- || pageIndex -->
                 <div class="col-8 d-flex justify-content-center">
-                    <ul class="list-unstyled d-flex align-items-center" v-if="pageIndex.length > 0">
-                        <li>
-                            <a href="#" class="text-black">&larr; Previous</a>
+                    <ul class="page-index list-unstyled d-flex align-items-center" v-if="pageIndex.length > 0">
+                        <li class="btn btn-sm me-1">
+                            <a href="#" class="text-black" @click="prevPage()">&larr; Previous</a>
                         </li>
-                        <li v-for="page in pageIndex" class="mx-1">
-                            <a href="#" class="text-black" @click="getProjects(page)">{{ page }}</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-black">Next &rarr;</a>
+                        <template v-for="page in pageIndex">
+                            <li class="mx-1 btn btn-sm" v-if="(page > this.currentPage - 3 && page < this.currentPage + 3) || (page > this.lastPage - 2)" :class="(page === this.currentPage) ? 'clicked' : ''">
+                                <a href="#" class="text-black" @click="getProjects(page)">{{ page }}</a>
+                            </li>
+                        </template>
+                        
+                        <li class="btn btn-sm ms-1">
+                            <a href="#" class="text-black" @click="nextPage()">Next &rarr;</a>
                         </li>
                     </ul>
                 </div>
@@ -71,5 +88,21 @@ export default{
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/variables' as *;
 
+    .page-index {
+        li {
+            background-color: $primary;
+            box-shadow: 0px 2px 5px rgba(0,0,0, 0.5);
+            opacity: 0.85;
+            
+            &.clicked {
+                box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5), 2px 1px 5px rgba(0, 0, 0, 0.5) inset;
+            }
+
+            a{
+                text-decoration: none;
+            }
+        }
+    }
 </style>
