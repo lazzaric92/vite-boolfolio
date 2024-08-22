147252e334ec4e410294938f9c779d82309243ba
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-22 12:00:41 +0200

    added pageIndex

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,29 +6,37 @@ export default{
     data(){
         return {
             projects: [],
+            pageIndex: [],
+            currentPage: ''
         }
     },
     components:{
         ProjectCard
     },
     methods: {
-        getProjects(){
+        getProjects(page){
             axios.get('http://127.0.0.1:8000/api/projects', {
                 params: {
-                
+                    page: page
                 }
             })
             .then((response) => {
-                console.log(response.data.results.data);
+                console.log(response.data.results);
                 this.projects = response.data.results.data;
+                const lastPage = response.data.results.last_page;
+                this.pageIndex = [];
+                for (let index = 1; index <= lastPage; index++){
+                    this.pageIndex.push(index);
+                }
+                this.currentPage = response.data.results.current_page;
             })
-            .catch(function (error) {
+            .catch((error) => {
                 console.log(error);
             }); 
         }
     },
     created(){
-        this.getProjects();
+        this.getProjects(1);
     }
 }
 </script>
@@ -39,6 +47,15 @@ export default{
         <div class="container-fluid p-4">
             <div class="row justify-content-center">
                 <ProjectCard class="col-8" v-for="project in projects" key="project.id" :title="project.title" :user="project.user" :date="project.date" :type="project.type" :description="project.description"/>
+
+                <!-- pageIndex -->
+                <div class="col-8 d-flex justify-content-center">
+                    <ul class="list-unstyled d-flex align-items-center">
+                        <li v-for="page in pageIndex" class="mx-1">
+                            <a href="#" class="text-black" @click="getProjects(page)">{{ page }}</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
